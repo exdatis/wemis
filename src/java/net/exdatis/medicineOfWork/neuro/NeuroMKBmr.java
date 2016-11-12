d0cb470a6eb1d6fcb9f37911aa4afe56c9b9aafb
morar <morar@localhost>
2016-11-12 18:49:34 +0100

    Neuro mkb.

---
+++ src/java/net/exdatis/medicineOfWork/neuro/NeuroMKBmr.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2016 morar
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package net.exdatis.medicineOfWork.neuro;
+
+import java.sql.Connection;
+import net.exdatis.wdb.CRUDdata;
+
+/**
+ * Dijagnostika vezana za MKB(ICD) u medicini rada(neuro-psihijatrija).
+ * @author morar
+ */
+public class NeuroMKBmr implements CRUDdata{
+    private int dijagnozaId;
+    private int dijagnozaPrijem;
+    private int dijagnozaMKB;
+    private String dijagnozaKomentar;
+    private boolean canEdit;
+
+    public NeuroMKBmr() {
+    }
+
+    public int getDijagnozaId() {
+        return dijagnozaId;
+    }
+
+    public void setDijagnozaId(int dijagnozaId) {
+        this.dijagnozaId = dijagnozaId;
+    }
+
+    public int getDijagnozaPrijem() {
+        return dijagnozaPrijem;
+    }
+
+    public void setDijagnozaPrijem(int dijagnozaPrijem) {
+        this.dijagnozaPrijem = dijagnozaPrijem;
+    }
+
+    public int getDijagnozaMKB() {
+        return dijagnozaMKB;
+    }
+
+    public void setDijagnozaMKB(int dijagnozaMKB) {
+        this.dijagnozaMKB = dijagnozaMKB;
+    }
+
+    public String getDijagnozaKomentar() {
+        return dijagnozaKomentar;
+    }
+
+    public void setDijagnozaKomentar(String dijagnozaKomentar) {
+        this.dijagnozaKomentar = dijagnozaKomentar;
+    }
+
+    public boolean isCanEdit() {
+        return canEdit;
+    }
+
+    public void setCanEdit(boolean canEdit) {
+        this.canEdit = canEdit;
+    }
+
+    @Override
+    public String insertRec(Connection connection) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String updateRec(Connection connection) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String deleteRec(Connection connection) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
